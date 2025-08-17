e55f1bb8d932a3f45b76ce0f58e6c571c511dfde
syamchandnair <syamchandnair@gmail.com>
2025-08-17 09:52:53 +0530

    Python  - Assignment 1 - Task 1 & 2

---
+++ assignment_01_task_02.py
@@ -0,0 +1,11 @@
+# ASSIGNMENT 1:
+# Module 2: Basic Python Concepts
+
+# Task 2: Create a Personalized Greeting
+
+vFirstName=input('Enter the first name: ')
+vLastName=input('Enter the last name: ')
+
+vFullName=vFirstName+" "+vLastName
+
+print ('Hello, ' +vFullName + '! Welcome to the Python program')
